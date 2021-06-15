0773c42e03beb40f458b82579540856203178531
wwmarkositrawe <wungielq@gmail.com>
2021-06-15 10:37:25 +0200

    MVP. two modes. accepting limit parameter

--- bimippsi.py
+++ bimippsi.py
@@ -1,15 +1,12 @@
 import random
-import time
 import os
-import itertools
-import threading
+import click
 import sys
-from threading import Timer
 from alive_progress import alive_bar, config_handler, bouncing_spinner_factory
 
 config_handler.set_global(length="35", bar="smooth",
                           force_tty=False, enrich_print=True)
-hng_spinner = bouncing_spinner_factory('🛴', 10, hiding=False)
+# hng_spinner = bouncing_spinner_factory('🛴', 10, hiding=False)
 os.system('')  # allows to print ANSI codes in terminal
 
 MAX_MAGIC_LEN = 10000
@@ -19,34 +16,58 @@ PREFIXES = ['pane', 'mono', 'dyne', 'mini', 'mine', 'tele', 'poli', 'semi',
             'mane', 'tone', 'pine', 'solo', 'pupu', 'titi', 'mumu', 'pipi',
             'para', 'nima', 'mana', 'tera', 'tite', 'pima', 'piba', 'breś',
             'tra', 'pra', 'sra', 'fra', 'bra', 'mra', 'bry', 'pry', 'sry',
-            'cim', 'pre', 'sraj', 'preś', 'miau', 'sple', 'sper']
+            'cim', 'pre', 'sraj', 'preś', 'miau', 'sple', 'sper', 'miaumu',
+            'a', 'plene']
+
+global li
+li = []
 
 
-def esc(code):
-    return f'\033[{code}m'
+@click.group(help="CLI app to create words meeting certain conditions")
+def cli():
+    pass
 
 
-def main():
-    select_mode()
+@cli.command("mode")
+@click.pass_context
+@click.option('--mode', type=click.Choice(['baps', 'premp'],
+                                          case_sensitive=False),
+              prompt="\nHEJ HEJ. By chciałeś/lubiałeś w trybie podstawowym (baps), czy dla cwaniaka (premp)")
+def select_mode(ctx, mode):
+    if mode == 'baps':
+        create_word_basic()
+        output_words(ctx, 'baps', WORDS_SIMPLE)
+    elif mode == 'premp':
+        create_word_avantgarde()
+        output_words(ctx, 'premp', WORDS_AVANGARDE)
 
 
-def select_mode():
-    modeString = input(
-        "\nHEJ HEJ. By chciałeś/lubiałeś w trybie podstawowym (0), czy dla cwaniaka (1) " +
-        "w ramach tzw. trybu premp, powiedz szczerze: ")
-    print()
-    if modeString in ("w trybie podstawowym", "0"):
-        create_word_basic(modeString)
-        output_words(WORDS_SIMPLE)
-    elif modeString in ("dla cwaniaka", "1", "premp"):
-        create_word_avantgarde(modeString)
-        output_words(WORDS_AVANGARDE)
-    else:
-        print(esc('31;1;4') + ("że co robi?") + esc(0) + ' cim oyunu \n')
+def output_words(ctx, mode, li):
+    try:
+        limit = click.prompt('ile chcesz wyrazow cwaniaku: ',
+                             type=click.IntRange(0, len(li)), default=20)
+        click.echo('\n')
+        with alive_bar(limit, calibrate=4, spinner="arrows") as bar:
+            for item in range(limit):
+                bar()
+
+        click.echo('\n')
+
+        for i in range(0, limit):
+            click.echo("{}: {}".format(i, li[i]))
+
+        click.echo('\nC + M + B 2o21!')
+        click.echo("\nBYYY")
+        click.echo("\na bapśmieni?")
+    except IndexError:
+        limit = 20
 
 
-def create_word_basic(modeString):
-    for i in range(0, MAX_MAGIC_LEN):
+def create_word_basic():
+    # possibilities = 3 * 2 * 3 * 1 * 2 * 1 * 1 * 2 = 72
+    possibilities = 72
+    finished = False
+    while not finished:
         letters = []
         first_letter = random.choice("bpm")
         letters.append(first_letter)
@@ -67,10 +88,17 @@ def create_word_basic(modeString):
         word = "".join(letters)
         if word not in WORDS_SIMPLE:
             WORDS_SIMPLE.append(word)
+        if len(WORDS_SIMPLE) == possibilities:
+            finished = True
+    li = WORDS_SIMPLE
+    return li
 
 
-def create_word_avantgarde(modeString):
-    for i in range(0, MAX_MAGIC_LEN):
+def create_word_avantgarde():
+    # possibilities =  len(PREFIXES) * (3 * 2 * 2 * 1 * 1 * 2) = 43 * 24 = 1032
+    possibilities = len(PREFIXES) * 24
+    finished = False
+    while not finished:
         letters = []
         prefix = random.choice(PREFIXES)
         first_letter = random.choice("bpm")
@@ -88,23 +116,11 @@ def create_word_avantgarde(modeString):
         word = prefix + "".join(letters)
         if word not in WORDS_AVANGARDE:
             WORDS_AVANGARDE.append(word)
-
-
-
-def output_words(li):
-    total_length = len(WORDS_SIMPLE) or len(WORDS_AVANGARDE)
-    sleep_time = random.randint(0, 1)/2147483647
-    with alive_bar(total_length, calibrate=5, spinner="arrows") as bar:
-        for item in li:
-            time.sleep(sleep_time)
-            bar()
-    print()
-    for item in li:
-        time.sleep(sleep_time)
-        print(item, end="... ", flush=True)
-    print('\n\n\x1b[6;30;42m' + 'C + M + B 2o21!' + '\x1b[0m')
-    print("\na bapśmieni...? a papsi...?")
+        if len(WORDS_AVANGARDE) == possibilities:
+            finished = True
+    li = WORDS_AVANGARDE
+    return li
 
 
 if __name__ == '__main__':
-    main()
+    cli(obj={})
