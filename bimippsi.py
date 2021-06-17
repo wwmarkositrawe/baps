95aaf614f378c27f0c49fd94ff69eaadfce41488
wwmarkositrawe <wungielq@gmail.com>
2021-06-17 12:31:41 +0200

    added colors. MVP

--- bimippsi.py
+++ bimippsi.py
@@ -2,6 +2,8 @@ import random
 import os
 import click
 import sys
+import numpy as np
+import time
 from alive_progress import alive_bar, config_handler, bouncing_spinner_factory
 
 config_handler.set_global(length="35", bar="smooth",
@@ -17,7 +19,7 @@ PREFIXES = ['pane', 'mono', 'dyne', 'mini', 'mine', 'tele', 'poli', 'semi',
             'para', 'nima', 'mana', 'tera', 'tite', 'pima', 'piba', 'breś',
             'tra', 'pra', 'sra', 'fra', 'bra', 'mra', 'bry', 'pry', 'sry',
             'cim', 'pre', 'sraj', 'preś', 'miau', 'sple', 'sper', 'miaumu',
-            'a', 'plene']
+            'a', 'plene', 'trata']
 
 global li
 li = []
@@ -44,7 +46,7 @@ def select_mode(ctx, mode):
 
 def output_words(ctx, mode, li):
     try:
-        limit = click.prompt('ile chcesz wyrazow cwaniaku: ',
+        limit = click.prompt('ile chcesz wyrazow cwaniaku [0-{}]: '.format(len(li)),
                              type=click.IntRange(0, len(li)), default=20)
         click.echo('\n')
         with alive_bar(limit, calibrate=4, spinner="arrows") as bar:
@@ -52,16 +54,35 @@ def output_words(ctx, mode, li):
                 bar()
 
         click.echo('\n')
-
+        
         for i in range(0, limit):
-            click.echo("{}: {}".format(i, li[i]))
+            click.echo(click.style("{}: {}".format(i, li[i]), fg=random_color(),
+                                   bg=random_color(), bold=True, reset=True))
 
-        click.echo('\nC + M + B 2o21!')
-        click.echo("\nBYYY")
-        click.echo("\na bapśmieni?")
+            # click.echo("{}: ".format(i) + click.style("{}".format(li[i]), fg=random_color(),
+            #                        bg=random_color(), bold=True, reset=True))
+
+        click.echo()
+        click.echo(click.style('C + M + B 2o21!', italic=True, reset=True))
+        click.echo()
+        click.echo(click.style("BYYY", reverse=True, reset=True))
+        click.echo()
+        click.echo(click.style("a bapśmieni?",
+                               strikethrough="True", reset=True))
     except IndexError:
         limit = 20
 
+
+def random_color():
+    # levels = range(32, 256, 32)
+    # return tuple(random.choice(levels) for _ in range(3))
+
+
+    # return [random.randint(0, 255),
+    #                  random.randint(0, 255), random.randint(0, 255)]
+
+
+    return tuple(np.random.randint(256, size=3))
 
 def create_word_basic():
     # possibilities = 3 * 2 * 3 * 1 * 2 * 1 * 1 * 2 = 72
